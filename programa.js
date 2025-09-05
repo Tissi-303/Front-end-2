1b801f1c51e784cadb4177da3d33bf9547ed2868
Tissi-303 <leticiasantosjp25@gmail.com>
2025-09-04 22:07:26 -0300

    Update programa.js

--- programa.js
+++ programa.js
@@ -1,52 +1,49 @@
-let totalAulaSemestre = parseInt(prompt("Informe o total de aulas do semestre: "));
-let totalFalta = parseInt(prompt("Informe o total de falta do aluno: "))
-let nota1 = parseFloat(prompt("Informe a primeira nota: "));
-let nota2 = parseFloat(prompt("Informe a segunda nota: "));
-let notaComplementar = parseFloat("Se houver, Informe a nota complementar")
 
-let  mediaComRecuperacao = ((mediaAluno + notaComplementar)/2).toFixed(2);
+console.log("Inicio");
 
-//Caclulando o percentual de presença
+let TotalAulaSemestre = parseInt(prompt("informe o tolal de aulas do semestre: "));
+let TotalFaltaAlunos = parseInt(prompt("informe o tolal de aulas que o aluno faltou: "));
+let PrimeiraNota = parseFloat(prompt("informe sua primeira nota: "));
+let SegundaNota = parseFloat(prompt("informe sua segunda nota: "));
+let MediaNota
+let ResultRec
 
-let percentualPresencaAluno = ((totalFalta/totalAulaSemestre)*100).toFixed(2);
+let TotalFalta = ((TotalFaltaAlunos / TotalAulaSemestre) * 100).toFixed(2);
 
-if (percentualPresencaAluno > 25) {
-    console.log("O aluno foi reprovado por falta")
-}
-
-// Calculo da media
-let mediaAluno = ((nota1 = nota2/2)).toFixed(2);
-
-if (mediaAluno >= 7){
-    console.log("Aprovado por média!")
-
-    else if (mediaAluno < 5)
-    console.log("Reprovado por média")
-
-    let  mediaComRecuperacao = ((mediaAluno + notaComplementar)/2).toFixed(2);
-
-        else if (mediaAluno >= 5 && < 7)
-    let notaComplementar = parseFloat("Se houver, Informe a nota complementar")mediaComRecuperacao
-
-    let  mediaComRecuperacao = ((mediaAluno + notaComplementar)/2).toFixed(2);
-
-        else if (mediaComRecuperacao >= 5)
-    console.log("Aprovado com a nota de recuperação!")
-
-        else
-        console.log("Rerprovado")
+if(TotalFalta > 25){
+    
+    console.log("Você foi reprovado por frequencia.")
 
 }
+else{
+    
+    let MediaNota = ((PrimeiraNota+SegundaNota)/2).toFixed(2);
+    console.log(MediaNota," Sua media final");
+    console.log(TotalFalta,"% de falta");
 
+    if(MediaNota >= 7){
+       
+        console.log("Você foi aprovado por nota e por frequencia");
+    
+    }
+    else if(MediaNota < 7 && MediaNota >= 5){
+        
+        console.log("Você foi aprovado por frequencia mas está de recuperação");
+        let ProvaRec = parseFloat(prompt("informe sua nota da recuperação: "));
+        let ResultRec = ((ProvaRec+MediaNota)/2).toFixed(2);
 
+        if(ResultRec >= 7){
+           console.log("Você passou na recuperação com nota", ResultRec);
+        }
+        else{
+            console.log("Você reprovou com nota", ResultRec);
+        }
+    }
+    
+    else{
+        
+        console.log("Você foi aprovado por frequencia mas está reprovado por nota");
+    
+    }
 
-
-console.log(totalAulaSemestre);
-console.log(totalFalta);
-console.log(percentualPresencaAluno);
-console.log(notaComplementar);
-console.log(mediaComRecuperacao);
-
-
-
-
+}
